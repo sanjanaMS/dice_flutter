2fe9d04decb2a511a3d6dffacf01d4418033d62b
sanjanaMS <56424429+sanjanaMS@users.noreply.github.com>
2021-05-06 01:22:36 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,10 +15,52 @@ void main() {
     ),
   );
 }
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
 
-class DicePage extends StatelessWidget {
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber=2;
+  int rightDiceNumber=3;
+  void interChangeDice(){
+    leftDiceNumber=Random().nextInt(6)+1;
+    rightDiceNumber=Random().nextInt(6)+1;
+  }
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return  Center(
+    child: Row(
+    children: [
+    Expanded(
+    // ignore: deprecated_member_use
+    child: TextButton(
+    child: Image.asset('images/dice$leftDiceNumber.png'),
+    onPressed: () {
+      setState(() {
+        interChangeDice();
+      });
+
+    },
+    ),
+    ),
+    Expanded(
+    // ignore: deprecated_member_use
+    child: TextButton(
+    child: Image.asset('images/dice$rightDiceNumber.png'),
+    onPressed: () {
+      setState(() {
+        interChangeDice();
+      });
+    },
+    ),
+    ),
+    ],
+
+    ),
+    );;
   }
 }
+
+
+
